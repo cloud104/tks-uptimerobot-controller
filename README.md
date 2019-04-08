be2ded101bfe7f3ec598f99052677069a17fc6d5
yurifrl <yurifl03@gmail.com>
2019-04-08 10:17:23 -0300

    feature: Kubebuilder init controller

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Simple Tenancy Aggregator
 
+## Comands to generated
+
 - `env GOPATH=$HOME/Workspace kubebuilder init --domain tks.sh`
-- `env GOPATH=$HOME/Workspace kubebuilder create api --group workloads --version v1beta1 --kind TenancyAggregator`
+- `env GOPATH=$HOME/Workspace kubebuilder create api --group monitors --version v1 --kind UptimeRobot --controller=true --resource=false`
+
+## TODO
+
+- Figure out the the queue stuff
---
+++ pkg/controller/uptimerobot/uptimerobot_controller.go
@@ -0,0 +1,97 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package uptimerobot
+
+import (
+	"context"
+	"fmt"
+
+	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new UptimeRobot Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileUptimeRobot{Client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("uptimerobot-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to UptimeRobot
+	err = c.Watch(&source.Kind{Type: &extensionsv1beta1.Ingress{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var _ reconcile.Reconciler = &ReconcileUptimeRobot{}
+
+// ReconcileUptimeRobot reconciles a UptimeRobot object
+type ReconcileUptimeRobot struct {
+	client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a UptimeRobot object and makes changes based on the state read
+// and what is in the UptimeRobot.Spec
+// +kubebuilder:rbac:groups=extensions,resources=ingresses,verbs=get;list;watch
+// +kubebuilder:rbac:groups=extensions,resources=ingresses/status,verbs=get
+func (r *ReconcileUptimeRobot) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	// Fetch the UptimeRobot instance
+	instance := &extensionsv1beta1.Ingress{}
+	err := r.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Object not found, return.  Created objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers.
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	// @TODO: Invoke someone here to post to a external source that can be queryed
+	fmt.Printf("%v\n", instance.Name)
+	fmt.Printf("%v\n", instance.ObjectMeta)
+
+	return reconcile.Result{}, nil
+}
